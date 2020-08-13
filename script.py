f2f018eef8bb7cef5828c1ef20e8725c1dbb1539
lmtls <zakoio6020@gmail.com>
2020-08-13 16:23:57 +0600

    added multiple face procession

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ def face_detect(image_path, cascade_path):
     image_grayscale = cv2.cvtColor(initial_image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(
         image_grayscale,
-        scaleFactor=1.05,
+        scaleFactor=scale_factor,
         minNeighbors=5,
         minSize=(20, 20)
     )
@@ -19,24 +19,37 @@ def face_detect(image_path, cascade_path):
     print("{} faces found".format(len(faces)))
     return faces      
 
-def image_flip(image_path, ):
-    image_obj = Image.open('temp/' + image_path)
-    flipped_image = image_obj.transpose(Image.FLIP_LEFT_RIGHT)
-    flipped_image.save('temp/flip_{}'.format(image_path))
+def image_flip(image_path):
+    try:
+        image_obj = Image.open('temp/half/' + image_path)
+        flipped_image = image_obj.transpose(Image.FLIP_LEFT_RIGHT)
+        flipped_image.save('temp/half/flip_{}'.format(image_path))
+    except:
+        pass
 
-def image_concat(image1, image2):
+def image_concat(image1, image2, image):
     final = Image.new('RGB', (image1.width + image2.width, image1.height))
     final.paste(image1, (0,0))
     final.paste(image2, (image1.width, 0))
-    final.save('temp/shronk_{}'.format(image1.filename[image1.filename.index('/') + 1:]))
+    final.save('temp/shronk/{}'.format(image))
 
-def temp_clear():
-    files = glob.glob('temp/*')
+def temp_clear(directory):
+    files = glob.glob('temp/' + directory + '*')
     for f in files:
         os.remove(f)
 
+def folder_create(name):
+    if not os.path.exists(name):
+        os.makedirs(name)
+
 image_path = sys.argv[1]
 cascade_path = sys.argv[2]
+scale_factor = float(sys.argv[3])
+
+folder_create('temp/')
+folder_create('temp/half/')
+folder_create('temp/shronk/')
+
 initial_image = cv2.imread(image_path)
 faces = face_detect(image_path,cascade_path)
 
@@ -44,28 +57,24 @@ print("{} faces found".format(len(faces)))
 
 for x,y,w,h in faces:
     roi_color_left = initial_image[y: y + h, x: x + w//2]
-    cv2.imwrite("temp/{}x{}.jpg".format(w, h), roi_color_left)
+    cv2.imwrite("temp/half/{}x{}x{}x{}.jpg".format(x,y,w,h), roi_color_left)
 
-for image in os.listdir('temp/'):
+temp_list = os.listdir('temp/half/')
+
+for image in temp_list:
     image_flip(image)
 
-temp_list = os.listdir('temp/')
-for image in temp_list:
-    try:
-        image1 = Image.open('temp/' + image)
-        image2 = Image.open('temp/flip_' + image)
-        temp_clear()
-        image_concat(image1, image2)     
-    except FileNotFoundError:
-        pass
+    image1 = Image.open('temp/half/' + image)
+    image2 = Image.open('temp/half/flip_' + image)
+    image_concat(image1, image2, image)     
+    
 
-temp_list = os.listdir('temp/')
-faces = zip(faces, temp_list)
+temp_list = os.listdir('temp/shronk/')
 
-for cords,path in faces:
-    s_img = cv2.imread('temp/' + path, -1)
+for x,y,w,h in faces:
+    path = str(x) +'x'+ str(y) +'x'+ str(w) +'x'+ str(h) + '.jpg'
+    s_img = cv2.imread('temp/shronk/' + path, -1)
     s_img = cv2.cvtColor(s_img, cv2.COLOR_RGB2RGBA).copy()
-    x,y,w,h = cords[0],cords[1],cords[2],cords[3]
     y1, y2 = y, y + s_img.shape[0]
     x1, x2 = x, x + s_img.shape[1]
 
@@ -76,8 +85,7 @@ for cords,path in faces:
         initial_image[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] +
                                   alpha_l * initial_image[y1:y2, x1:x2, c])
 cv2.imwrite('final/{}x{}.jpg'.format(w,h), initial_image)
-
-
-
+temp_clear('half/')
+temp_clear('shronk/')
 
 
